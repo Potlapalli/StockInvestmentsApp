efc9a183bcdd06345798ad923b5a8399e1b52e4d
Ananth Narsimha Rao Potlapalli <potlapalliananth@gmail.com>
2021-09-08 23:48:31 -0500

    Commit changes

--- StockInvestmentsUI/Static/Endpoints.cs
+++ StockInvestmentsUI/Static/Endpoints.cs
@@ -7,7 +7,11 @@ namespace StockInvestmentsUI.Static
 {
     public static class Endpoints
     {
+#if DEBUG
         public static string BaseUrl = "http://localhost:51044/";
+#else
+        public static string BaseUrl = "https://stockinvestmentsapi20210908221344.azurewebsites.net/";
+#endif
         public static string CurrentPositionsEndpoint = $"{BaseUrl}api/currentPositions/";
         public static string CurrentPositionsFilteredByTotalAmountEndpoint 
             = $"{BaseUrl}api/currentPositions/filterByTotalAmount?totalAmountGreaterThan=";
